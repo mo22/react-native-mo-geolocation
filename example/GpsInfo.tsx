c22debffea3b153d8d7bd67758b8c027f4366b97
Moritz Moeller <mm@mxs.de>
2019-09-22 20:44:48 +0200

    ..

--- example/GpsInfo.tsx
+++ example/GpsInfo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ScrollView, View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { NavigationActions, NavigationInjectedProps } from 'react-navigation';
-import { Geolocation, GeolocationResult, GeolocationPermissionStatus, GeolocationAccuracy } from 'react-native-mo-geolocation';
+import { Geolocation, GeolocationResult, GeolocationAccuracy } from 'react-native-mo-geolocation';
 import { Releaseable } from 'mo-core';
 import { Fs } from 'react-native-mo-fs';
 
@@ -26,7 +26,6 @@ export let subscription: Releaseable|undefined;
 export interface GeolocationPageState {
   error?: Error|string;
   position?: GeolocationResult;
-  permission?: GeolocationPermissionStatus;
   background: boolean;
   indicateBackground: boolean;
   accuracy: GeolocationAccuracy;
@@ -142,37 +141,6 @@ export default class GpsTest extends React.PureComponent<NavigationInjectedProps
             />
           </View>
         )}
-
-        <View style={{ marginTop: 10 }}>
-          <Text>Permissions:</Text>
-          <ListItem
-            title="permission"
-            rightTitle={`${this.state.permission}`}
-            onPress={() => {
-              Geolocation.showSettings();
-            }}
-          />
-          <ListItem
-            title="request permissions"
-            onPress={async () => {
-              const res = await Geolocation.requestPermissions({ background: this.state.background });
-              this.setState({ permission: res });
-            }}
-          />
-          <ListItem
-            title="get permission status"
-            onPress={async () => {
-              const res = await Geolocation.getPermissionStatus({ background: this.state.background });
-              this.setState({ permission: res });
-            }}
-          />
-          <ListItem
-            title="open settings"
-            onPress={async () => {
-              await Geolocation.openSettings();
-            }}
-          />
-        </View>
 
         <View style={{ marginTop: 10 }}>
           <Text>Settings:</Text>
--- example/Menu.tsx
+++ example/Menu.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { NavigationActions, NavigationInjectedProps } from 'react-navigation';
+import { Geolocation, GeolocationPermissionStatus } from 'react-native-mo-geolocation';
 
-export default class Menu extends React.PureComponent<NavigationInjectedProps> {
+export default class Menu extends React.PureComponent<NavigationInjectedProps, Menu['state']> {
+  public state: {
+    permission?: GeolocationPermissionStatus;
+    background?: boolean;
+  } = {
+  };
+
   public render() {
     return (
       <ScrollView>
@@ -15,6 +22,44 @@ export default class Menu extends React.PureComponent<NavigationInjectedProps> {
             this.props.navigation.dispatch(NavigationActions.navigate({ routeName: 'GpsInfo' }));
           }}
         />
+
+        <View style={{ marginTop: 10 }}>
+          <Text>Permissions:</Text>
+          <ListItem
+            title="background"
+            switch={{
+              value: this.state.background,
+              onValueChange: (value) => this.setState({ background: value }),
+            }}
+          />
+          <ListItem
+            title="permission"
+            rightTitle={`${this.state.permission}`}
+            onPress={() => {
+              Geolocation.showSettings();
+            }}
+          />
+          <ListItem
+            title="request permissions"
+            onPress={async () => {
+              const res = await Geolocation.requestPermissions({ background: this.state.background });
+              this.setState({ permission: res });
+            }}
+          />
+          <ListItem
+            title="get permission status"
+            onPress={async () => {
+              const res = await Geolocation.getPermissionStatus({ background: this.state.background });
+              this.setState({ permission: res });
+            }}
+          />
+          <ListItem
+            title="open settings"
+            onPress={async () => {
+              await Geolocation.openSettings();
+            }}
+          />
+        </View>
 
       </ScrollView>
     );
